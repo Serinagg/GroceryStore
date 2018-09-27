ece7a8572f2f2a9b2fe093aa50e33386c00288f1
serinagg <sgarcia14@mail.csuchico.edu>
2018-09-26 20:30:17 -0700

    loop created

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@ int main()
 {
   string list[5]; //array of 5 strings
   int numItems = 0;
-  char input;
+  char input; 
 
   cout<<"\n==GROCERY LIST MANAGER==";
-  cout<<"\nEnter your choice: ";
-  cout<<"\n (A)dd an item";
-  cout<<"\n (Q)uit";
-  cout<<"\nYour choice (A/Q): ";
-  cin>>input;
+do {
+        cout<<"\nEnter your choice: ";
+        cout<<"\n (A)dd an item";
+        cout<<"\n (Q)uit";
+        cout<<"\nYour choice (A/Q): ";
+        cin>>input;
+
+          cout<<list[0]<<"\n"<<list[1]<<"\n"<<list[2]<<"\n"<<list[3]<<"\n"<<list[4];        
+} while( input != 'q' && input != 'Q');
+ 
 
   return 0;
 }
