016ae413b2d91838fe64f81c31ee4e0f23564e59
serinagg <sgarcia14@mail.csuchico.edu>
2018-10-10 16:41:10 -0700

    deleting groceries

--- main.cpp
+++ main.cpp
@@ -14,27 +14,36 @@ int main()
                 cout<<"\n==GROCERY LIST MANAGER==";
                 cout<<"\nEnter your choice: ";
                 cout<<"\n (A)dd an item";
+                if (list.size() > 0)
+                {
+                        cout<<"\n (D)elete last item";
+
+                }
                 cout<<"\n (Q)uit";
-                cout<<"\nYour choice (A/Q): ";
+                cout<<"\nYour choice (A/Q):";
                 cin>>input;
 
                 if(input =='a' || input =='A')
                 {
-                        cout<<"What is the item?\n"<<endl;
+                        cout<<"What is the item?"<<endl;
                         cin>>item;
                         list.push_back(item);
-                } 
+                } else if (input == 'd' || input == 'D')
+                {
+                        item = list[list.size()-1];
+                        list.pop_back();
+                        cout<<item<<" was delete from the list.";
+                }
+
+        } while(input != 'q' && input != 'Q');
+
+        for (int i = 0; i< list.size(); i++)
+        {
+                cout<<i + 1<<" "<<list[i]<<endl;
         }
-        while(input != 'q' && input != 'Q');
         if (list.size() == 0)
         {
                 cout<<"No items to buy!"<<endl;
-        } else{
-                cout<<"==ITEMS TO BUY=="<<endl;
-                for (int i = 0; i< list.size(); i++)
-                {
-                        cout<<i+1<<" "<<list[i]<<endl;
-                }
         }
 
         return 0;
